091ad8ca4dbab16a629dae4dec7899a8b21dfe83
Sanieye <88657760+Sanieye@users.noreply.github.com>
2021-12-09 22:35:04 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js"
+        integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+    <style>
+        .block1 {
+            max-width: 100%;
+            margin: 20px;
+            height: auto;
+
+        }
+        .text {
+            margin-bottom: 30px;
+
+        }
+    </style>
+
+
+
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="block1"><img src="https://i2.wp.com/travelblog.expedia.co.th/wp-content/uploads/2018/08/cover-trekking.jpg?fit=1200%2C550&ssl=1" class="img-fluid"></div>
+        <div class="row">
+            <div class="col-6" height:  style="text-align: center; height: auto;">
+                <img src="https://www.nairobroo.com/wp-content/uploads/2018/07/%E0%B9%80%E0%B8%82%E0%B8%B2%E0%B9%80%E0%B8%A2%E0%B9%87%E0%B8%99-1.jpg" class="img-fluid" /> 
+                <h1>การถ่ายภาพ</h1>
+                <p>การถ่ายภาพ" Photography หมายถึง การบันทึกเหตุการณ์ใดเหตุการณ์หนึ่ง ณ จุดเวลาใดเวลาหนึ่ง ในสถานที่นั้น 
+                    โดยเป็นการเก็บสภาพแสงไว้ ลงบนวัตถุไวแสงทางเคมี เช่น ฟิล์มถ่ายภาพ หรือวงจรไฟฟ้าไวแสง CMOS , CCD เป็นต้น 
+                    โดยบรรจุอยู่ในอุปกรณ์ที่เราเรียกว่ากล้องถ่ายรูป ด้วยเทคนิคการถ่ายภาพแบบต่างๆ หลังจากนั้นการถ่ายภาพจะเปล่ียนสภาพแสงที่บันทึกไว้เหล่านั้น 
+                    กลับมาเป็นภาพได้อีกครั้งหนึ่ง โดยผ่าน กระบวนการล้างอัดภาพการถ่ายภาพจากศัพท์ภาษาอังกฤษว่าPhotography 
+                    โฟโตกราฟฟีเป็นรากศัพท์ มาจากภาษากรีก 2 คา ได้แก่คาว่า Phos ซึ่งหมายถึง แสงสว่าง และคาว่า Graphein หมายถึง การวาดหรือ 
+                    การเขียน เมื่อรวมกันจึงหมายถึง การเขียนหรือวาดด้วยแสงสว่าง เรียกรวมๆ ว่าเทคนิคการถ่ายภาพน่ันเอ
+                </p>
+                
+
+            </div>
+
+            <div class="col-3">
+                <img src="https://lh3.googleusercontent.com/proxy/BJoPCZHQWR5qqf527GtgR3ucoEc2uu6eGUU_fdbYjPDssCqJb0rKBaV7zG7g1vyeJ1C1cb1w8agk3Xt2WnxrU4VrgTaXFFPCrFztJM3mwXauMe29r97KwDq-" class="img-fluid" />
+                <p>
+                    พิชิตยอดเขาโมโกจู อุทยานแห่งชาติแม่วงก์ จังหวัดกำแพงเพชร <br/>
+                    อุทยานแห่งชาติแม่วงก์ ครอบคลุมพื้นที่ 2 จังหวัด คือ กำแพงเพชรและนครสวรรค์ และในพื้นที่อุทยานฯ แห่งนี้ เป็นที่ตั้งของ “ยอดเขาโมโกจู” ยอดเขาที่ได้ขึ้นชื่อว่าสูงที่สุดในอุทยานแห่งชาติแม่วงก์ ด้วยความสูง 1,964 เมตร เหนือระดับน้ำทะเลปานกลาง ช่วงระหว่างเดือนพฤศจิกายน-กุมภาพันธ์ของทุกปี ทางอุทยานฯ จะเปิดเส้นทางเดินป่าเพื่อให้นักท่องเที่ยวได้พิชิตยอดเขาโมโกจู
+                </p>
+            </div>
+            <div class="col-3">
+                <img src="https://f.tpkcdn.com/images-720/4c585d3ce763fd816a64729ab83a55af.JPG" class="img-fluid" />
+                <p>
+                    ยอดดอยโมโกจู บรรยากาศโคตรดีอ่ะ <br/>
+                    อุทยานแห่งชาติแม่วงก์ นับได้ว่าเป็นมีผืนป่าที่มีความอุดมสมบูรณ์แห่งหนึ่ง มีพันธุ์ไม้ที่สำคัญมากมาย อาทิเช่น มะค่าโมง สัก ประดู่ ยางแดง เต็งรัง เป็นตัน อีกทั้งยังมีสัตว์ป่าที่หายาก เช่น ช้างป่า กระทิง เสือ กวาง เก้ง หมี แมวลาย และนกต่างๆ โดย ด้วยสภาพเส้นทางเดินขึ้นเขา ที่มีความลาดชันไม่ต่ำกว่า 60 องศา และใช้เวลาในการเดินทางไป – กลับ ใช้ระยะเวลา 5 วัน ซึ่งต้องพักแรมในป่าตามจุดที่ได้มีการกำหนดไว้ นักท่องเที่ยวจึงต้องเตรียมร่างกายให้พร้อมก่อนออกเดินทางจริ
+                </p>
+
+            </div>
+            <div class="col-6" style="margin-bottom: 10%;"> 
+        </div>
+    <div class="col-6" style="margin-bottom: 30%;" >
+        <button id="btnBack"> back </button>
+        <div id="main">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                    </tr>
+                </thead>
+                <tbody id="tblPost">
+                </tbody>
+            </table>
+        </div>
+        <div id="detail">
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>UserID</th>
+                    </tr>
+                </thead>
+                <tbody id="tblDetails">
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+</div>
+</div>
+
+</body>
+<script>
+    function showDetails(id) {
+        $("#main").hide();
+        $("#detail").show();
+        var url = "https://jsonplaceholder.typicode.com/posts/" + id
+        $.getJSON(url)
+            .done((data) => {
+                console.log(data);
+                var line = "<tr id='detailROW'";
+                line += "><td>" + data.id + "</td>"
+                line += "<td><b>" + data.title + "</b><br/>"
+                line += data.body + "</td>"
+                line += "<td>" + data.userId + "</td>"
+                line += "</tr>";
+                $("#tblDetails").append(line);
+            })
+            .fail((xhr, err, status) => {
+            })
+    }
+    function LoadPosts() {
+        var url = "https://jsonplaceholder.typicode.com/posts"
+        var i = 0;
+        $.getJSON(url)
+            .done((data) => {
+                $.each(data, (k, item) => {     
+                    i++;
+                    var line = "<tr>";
+                    line += "<td>" + item.id + "</td>"
+                    line += "<td><b>" + item.title + "</b><br/>"
+                    line += item.body + "</td>"
+                    line += "<td><button onClick='showDetails(" + item.id + ");'>Link</button></td>"
+                    line += "</tr>";
+                    $("#tblPost").append(line);
+                    if (i == 10) {
+                        loadPost().stop();
+                    };
+                });
+                $("#main").show();
+            })
+            .fail((xhr, err, status) => {
+            })
+    }
+    $(() => {
+        LoadPosts();
+        $("#detail").hide();
+        $("#btnBack").click(() => {
+            $("#main").show();
+            $("#detail").hide();
+            $("#detailROW").remove();
+        });
+    })
+</script>
+
+
+<footer >
+    <footer class="text-center text-white fixed-bottom" style="background-color: #21081a;">
+        <div class="container p-4"></div>
+          © 22 01 2545:
+         <p>631109 sanita jitturat</p>
+        </div>
+      </footer>
+</footer>
+    
+
+</html>
+ 
